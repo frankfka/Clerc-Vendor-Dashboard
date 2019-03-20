25aef23c5ae78c861b273ce672a61f5e48e33eb1
Frank Jia <jiafrank98@gmail.com>
2019-03-19 19:47:11 -0700

    parsing url params done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+            <Route exact path="/setup" component={Setup} />
+            <Route path="/setup/stripe-redirect" component={StripeRedirect} />
+        </Router>
       </div>
     );
   }
+}
+
+function Setup() {
+  return (
+    <div>
+      <h2>Setup</h2>
+    </div>
+  );
+}
+
+function StripeRedirect({location}) {
+
+  const queryParser = require('query-string');
+  const queries = queryParser.parse(location.search);
+  console.log(queries);
+
+  let text = "no params"
+  
+  if (queries.error) {
+    text = "Error: " + queries.error
+  } else if (queries.code) {
+    text = "Code: " + queries.code
+  }
+
+  return (
+    <div>
+      <h2>{text}</h2>
+    </div>
+  );
 }
 
 export default App;
